bf0dc7f1f86ad588a10e2dcf68d05fb5c8ad2c2c
Willem Huiskamp <willemhuiskamp88@gmail.com>
2019-11-26 16:56:25 +0100

    updated isolated cell checking

--- 3_check_channels/chk_cells.py
+++ 3_check_channels/chk_cells.py
@@ -9,9 +9,16 @@ import numpy as np
 import copy as cp
 
 # For testing, create dummy o_mask array with isolated cells
+# Normal square block
 o_mask = np.full([20,20],1)
 o_mask[0::,0] = 0; o_mask[0::,19] = 0; o_mask[0,0::] = 0; o_mask[19,0::] = 0;
 o_mask[11::,9] = 0; o_mask[11,9::] = 0;  #o_mask[11,10] = 1;
+
+# More complex shape
+o_mask = np.full([20,20],1)
+o_mask[0::,0] = 0; o_mask[0::,19] = 0; o_mask[0,0::] = 0; o_mask[19,0::] = 0;
+o_mask[18,9] = 0; o_mask[17,10:16] = 0; o_mask[16,16] = 0; o_mask[16,18] = 0;
+o_mask[15,17] = 0;
 
 def chk_cells(o_mask,chng_mask):
     # This function checks the ocean mask for cells or groups of cells isolated
@@ -46,8 +53,8 @@ def ID_iso_cells(row,col,o_mask,turn1,stop,grd):
     # different directions (defined in turn1). If cells are isolated, one 
     # direction will lead to the open ocean (for which a stop criteria is 
     # implemented) and the other will identify the isolated cells. 
-    # In the event that cells are not 
-    # isolated, the function will not return anything.
+    # In the event that cells are not isolated, the function will not 
+    # return anything.
     # In:       row - latitude index
     #           col - logitude index
     #        o_mask - ocean mask
@@ -61,13 +68,22 @@ def ID_iso_cells(row,col,o_mask,turn1,stop,grd):
     dir_1 = cp.deepcopy(new_dir)
     iso_mask = np.full(o_mask.shape,0)
     
+    # Extra check is needed if ocean is isolated by land cells only connected
+    # by their vertices. The square tracing algorithm cannot normally handle this.
     while count < stop:
         if o_mask[row_new,col_new] == 0:
             turn = 'right'
-        else:
+        elif o_mask[row_new,col_new] == 1 and 1 not in iso_mask:
             turn = 'left'
             iso_mask[row_new,col_new] = 1
             count += 1
+        elif o_mask[row_new,col_new] == 1 and sides_chk(row_new,col_new,iso_mask):
+            turn = 'left'
+            iso_mask[row_new,col_new] = 1
+            count += 1
+        elif o_mask[row_new,col_new] == 1 and not sides_chk(row_new,col_new,iso_mask):
+            turn = 'right'
+            
         row_new, col_new, new_dir = update_orientation(new_dir,row_new,col_new,turn,grd)
         
         # If we arrive back at the initial cell in the same manner, we've
@@ -130,22 +146,77 @@ def update_orientation(old_dir,row,col,turn,grd):
     row = idx[0]; col = idx[1]
 
     # Check if tracker is crossing prime meridian
-    if col == grd[0]:
+    if col == grd[1]:
         col = 0
     if col == -1:
-        col = grd[0]
+        col = grd[1]-1
     # ... or crossing the polar fold
-    if row == grd[1]:
-        row = grd[1]-1
-        col = (grd[0]-1)-col
+    if row == grd[0]:
+        row = grd[0]-1
+        col = (grd[1]-1)-col
 
      
     return row, col, new_dir
+
+def sides_chk(row,col,data):
+    # This checks whether an ocean cell is in fact within the same land-bounded
+    # region as other ocean cells, as the tracing algorithm can fail when ocean
+    # is bounded by land where only vertices of the cells are touching.
+    # Checks/ corrections are included in case prime meridian/ polar fold are 
+    # crossed.
+    # In:    row - Latitude index
+    #        col - Longitude index
+    #       data - Mask of cells identified by the ID_iso_cells function. Ocean 
+    #              cells are marked as 1, land as 0
+    # Out: flag  - When true, these ocean cells can said to be connection.
+    #              The opposite is true when false.
+    
+    # For cells not interacting with the polar fold
+    if row < data.shape[0]-1:
+        if col == data.shape[1]-1:
+            col_m1 = col-1; col_p1 = 0
+        elif col == 0:
+            col_p1 = 1; col_m1 = data.shape[1]-1
+        else:
+            col_m1 = col-1; col_p1 = col+1
+        if row == 0:
+            row_m1 = 0; row_p1 = 1
+        else:
+            row_m1 = row - 1; row_p1 = row+1
+            
+        if data[row_p1,col] == 1 or data[row_m1,col] == 1 \
+            or data[row,col_m1] == 1 or data[row,col_p1] == 1:
+            return True
+        else:
+            return False
+    # For cell at the polar fold, row_p1 will lie on other side. ie: same row,
+    # different col.    
+    elif row == data.shape[0]-1:
+        if col == data.shape[1]-1:
+            col_m1 = col-1; col_p1 = 0
+        elif col == 0:
+            col_p1 = 1; col_m1 = data.shape[1]-1
+        else:
+            col_m1 = col-1; col_p1 = col+1
+        row_m1 = row-1
         
-def fix_iso_cells(iso_mask):
+        if data[row,(data.shape[1]-1)-col] == 1 or data[row_m1,col] == 1 \
+            or data[row,col_m1] == 1 or data[row,col_p1] == 1:
+            return True
+        else:
+            return False  
+    
+
+def fix_iso_cells(iso_mask,chng_mask):
     # This function takes a group of isolated ocean cells, examines their 
     # collective properties and determines whether or not to leave them alone 
     # or fill them in.
+    # In: iso_mask - Mask showing the location of isolated cells associated with
+    #                a single changed gridcell
+    #     chng_mask - Mask showing which cells will change from land-sea or vice
+    #                 versa.
+    # Out: chng_mask - Updated change mask.
+    
     
     
     
@@ -154,12 +225,16 @@ def fix_iso_cells(iso_mask):
     
     
     return
-      
+grd = o_mask.shape      
+
 test = ID_iso_cells(11,10,o_mask,'right',40,grd)        
 if test is None:     
     test = ID_iso_cells(11,10,o_mask,'left',40,grd)
 
-        
+test = ID_iso_cells(18,9,o_mask,'right',40,grd)        
+if test is None:     
+    test = ID_iso_cells(18,9,o_mask,'left',40,grd)
+     
         
         
         
