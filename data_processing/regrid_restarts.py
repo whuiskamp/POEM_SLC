7c386c1136b05f0193b62f3c5639db6e7be8ba34
Willem Huiskamp <willemhuiskamp88@gmail.com>
2023-09-27 17:04:18 +0200

    Overhaul - lots of errors

--- data_processing/regrid_restarts.py
+++ data_processing/regrid_restarts.py
@@ -12,55 +12,56 @@ import glob
 import subprocess as sp
 
 def regrid_rest(path):
-	# This function reads in the experiment path and regrids the PISM and, when necessary, the VILMA restart file to the ocean grid
-	# Regridding is done using cdo and employs second-order conservative method. This script requires a master topography file
-	# to be stored in the 'path' directory (typically named SLC). It should include terrestrial topography as well as bathymetry
-	# as it exists at the beginning of the simulation.
+	# This function reads in the experiment path and regrids the PISM (pism_res.nc) and, when necessary, the VILMA restart (rsl.nc) 
+	# file to the ocean grid. Regridding is done using cdo and employs second-order conservative method. 
+	# This script requires a master topography file to be stored in the 'path' directory (typically named SLC). 
+	# It should include terrestrial topography as well as bathymetry as it exists at the beginning of the simulation.
 	# Next, if this is a VILMA coupling step, new topography field is calculated and saved to netCDF
 	#  In: path 		- the path of the experiment directory's coupling data folder
 	# Out: vilma2mom.nc - VILMA restart file on ocean grid
-	#	   	pism2mom.nc - PISM restart file on ocean grid
+	#	   pism2mom.nc  - PISM restart file on ocean grid
 	#      topog_new.nc - Default topography updated with VILMA data
 
 	# First, check if this is a VILMA coupling step
-    if os.path.isfile(str(path)+'rsl.nc'): # Change this path later for production runs (should not link to 'test_data')
-        print('Relative sea level file found. Regridding to ocean grid ...')
-        sp.run(['cdo', 'remapcon2,MOM.res.nc', 'rsl.nc', 'vilma2mom.nc'])
-        VILMA_couple = True
+	if os.path.isfile(str(path)+'rsl.nc'):
+		print('Relative sea level file found. Regridding to ocean grid ...')
+		sp.run(['cdo', 'remapcon2,'+str(path)+'MOM.res.nc', str(path)+'rsl.nc', str(path)+'vilma2mom.nc'])
+		VILMA_couple = True
 	else:
 		print('Relative sea level file not found. Proceeding to ice sheet input ...')
     # Read PISM restart file    
-    try:
-        sp.run(['cdo', 'remapcon2,'+str(path)+'MOM.res.nc', '+str(path)+'pism_res.nc, +str(path)+'vilma2mom.nc'])
-    except:
-        s = ("PISM extra file '{}' can't be found! ")
-        raise FileNotFoundError( s.format(args.PISM_extra_file) )
+	if os.path.isfile(str(path)+'pism_res.nc'):
+		print('PISM restart file found. Regridding to ocean grid ...')
+		sp.run(['cdo', 'remapcon2,'+str(path)+'MOM.res.nc', str(path)+'pism_res.nc', str(path)+'pism2mom.nc'])
+	else:
+		print('PISM restart file not found.')
 	
 
-    if VILMA_couple:
-    	sp.run(['cp', str(path)+'../INPUT/topog.nc', str(path)+'topog_new.nc'])
-    	master_topo = CDF(str(path)+'master_topo.nc','r',) # Import master topog. on MOM grid
-    	VILMA_data  = CDF(str(path)+'vilma2mom.nc','r',) # Import VILMA restart on MOM grid
-    	topo_m = master_topo.variables['depth'][:,:]; master_topo.close()  
-    	slr    = VILMA_data.variables['slr'][-1,:,:]; VILMA_data.close() # Note that VILMA appends restarts, so we always select the most
-    	# recent value
-    	depth_new = topo_m - slr
+    # if VILMA_couple:
+    # 	sp.run(['cp', str(path)+'../INPUT/topog.nc', str(path)+'topog_new.nc'])
+    # 	master_topo = CDF(str(path)+'master_topo.nc','r',) # Import master topog. on MOM grid
+    # 	VILMA_data  = CDF(str(path)+'vilma2mom.nc','r',) # Import VILMA restart on MOM grid
+    # 	topo_m = master_topo.variables['depth'][:,:]; master_topo.close()  
+    # 	slr    = VILMA_data.variables['slr'][-1,:,:]; VILMA_data.close() # Note that VILMA appends restarts, so we always select the most
+    # 	# recent value
+    # 	depth_new = topo_m - slr
 
-    	topog_new = CDF(str(path)+'topog_new.nc','r+')   # Import what will be the new topogrpahy file
-    	depth     = topog_new.variables['depth'][:,:]
-    	depth[:,:]= depth_new
-    	topog_new.close()
+    # 	topog_new = CDF(str(path)+'topog_new.nc','r+')   # Import what will be the new topogrpahy file
+    # 	depth     = topog_new.variables['depth'][:,:]
+    # 	depth[:,:]= depth_new
+    # 	topog_new.close()
 
-    else:
-    	sp.run(['cp', str(path)+'INPUT/topog.nc', str(path)+'topog_new.nc']) # Doesn't need to be updated, just copy the file as-is.
+    # else:
+    # 	sp.run(['cp', str(path)+'INPUT/topog.nc', str(path)+'topog_new.nc']) # Doesn't need to be updated, just copy the file as-is.
 
-    # Finally, we create a .nc file which tracks the history of the model topography & bathymetry at each coupling time-step
+    # This all needs to be moved to prep_restarts.py
+	# Finally, we create a .nc file which tracks the history of the model topography & bathymetry at each coupling time-step
     # This file is updated at the end of the SLC operations (changes due to column thickness and ice sheets must be accounted for)
-    if os.path.isfile(str(path)+'../history/topog_history.nc') == False: # At the start of a run, we need to creat this file as it doesn't exist yet
-    	topog = CDF(str(path)+'INPUT/topog.nc','r');                  # The current topog file will represent the first time-step
-    	depth = topo.variables['depth'][:,:]; topog.close()
+    if os.path.isfile(str(path)+'/../history/topog_history.nc') == False: # At the start of a run, we need to creat this file as it doesn't exist yet
+    	topog = CDF(str(path)+'/../INPUT/topog.nc','r');                  # The current topog file will represent the first time-step
+    	depth = topog.variables['depth'][:,:]; topog.close()
 
-    	hist  = CDF.Dataset('topog_history.nc', 'w')				  # Create the topog_history file and add metadata
+    	hist  = CDF.Dataset('topog_history.nc', 'w')				  	  # Create the topog_history file and add metadata
 		hist.createDimension('nx', depth.shape[1])
 		hist.createDimension('ny', depth.shape[0])
 		hist.createDimension('time', None)
