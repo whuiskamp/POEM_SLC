ab2355e95d2700b917bc1cac3a2745c21dc8c170
Willem Huiskamp <willemhuiskamp88@gmail.com>
2023-11-14 14:04:07 +0100

    Finalising first ver. of run script

--- run_slc.sh
+++ run_slc.sh
@@ -24,7 +24,7 @@
 
 # ----------- Define paths ----------
 diag_dir      = history
-SLC_work      = mkdir $PWD/SLC
+SLC_work      = $PWD/SLC
 SLR_tool      = /p/projects/climber3/huiskamp/POEM/work/slr_tool
 runoff_regrid = /p/projects/climber3/huiskamp/regrid_runoff # Maybe incorporate this inside the tool??
 
@@ -39,17 +39,31 @@ if [ $START != 1 ]; then
 fi
 
 DO_PISM        = False  # Will this simulation include external forcing from PISM
-DO_VILMA       = False  # Will this simulation include external forcing from VILMA 
+DO_VILMA       = False  # Will this simulation include external forcing from VILMA
+
+mkdir -p $SLC_work
 
 OM4_run(){
     # Run MOM6-SIS2
     echo "Running MOM6-SIS2..."
     srun --propagate=ALL ./MOM6_SIS2 > MOM_log 2>&1
-    mkdir -p "${diag_dir}/MOM6_run${RUN}"
+    mkdir -p "${diag_dir}/MOM6_run_${RUN}"
     
-    mv *.nc sbatch.62* MOM_parameter_doc* time_stamp.out CPU_stats ocean.stats logfile* MOM_log available_diags.* V_velocity_truncations U_velocity_truncations SIS_parameter_doc.layout SIS.available_diags SIS_fast.available_diags SIS_parameter_doc.debugging SIS_parameter_doc.short SIS_parameter_doc.all MOM_IC.nc seaice.stats "${diag_dir}/MOM6_run${RUN}"
+    # Copy param. files to working dir...
+    cp -a MOM_parameter_doc.all SIS_parameter_doc.all $SLC_work
+
+    # Move diagnostics and log files to history directory
+    mv *.nc sbatch.62* MOM_parameter_doc* time_stamp.out CPU_stats ocean.stats logfile* MOM_log available_diags.* \
+    V_velocity_truncations U_velocity_truncations SIS_parameter_doc.layout SIS.available_diags SIS_fast.available_diags \
+    SIS_parameter_doc.debugging SIS_parameter_doc.short SIS_parameter_doc.all MOM_IC.nc seaice.stats "${diag_dir}/MOM6_run_${RUN}"
     
-    cp -a MOM_input input.nml MOM_override diag_table field_table SIS2_memory.h MOM_memory.h SIS_input SIS_override static_input.nml README "${diag_dir}/MOM6_run${RUN}"
+    cp -a MOM_input input.nml MOM_override diag_table field_table SIS2_memory.h MOM_memory.h SIS_input SIS_override \
+    static_input.nml README "${diag_dir}/MOM6_run_${RUN}"
+
+    # Move restarts to working dir (also create a backup)...
+    cp -ar RESTART/  "${diag_dir}/MOM6_run_${RUN}"
+    cp -a RESTART/{MOM.res.nc,ice_model.res.nc} $SLC_work
+    cp -a INPUT/topog.nc $SLC_work
 }
 
 slc_run(){
@@ -78,18 +92,17 @@ do
     # Run poem for coupling timestep
     OM4_run
     if [$RUN == 1]; then
-       cp ${diag_dir}/MOM6_run${RUN}/{MOM_parameter_doc.all,SIS_parameter_doc.all} $SLC_work
-       cp ${diag_dir}/MOM6_run${RUN}/*.ocean_static.nc $SLC_work/ocean_static.nc
+       cp ${diag_dir}/MOM6_run_${RUN}/{MOM_parameter_doc.all,SIS_parameter_doc.all} $SLC_work
+       cp ${diag_dir}/MOM6_run_${RUN}/*.ocean_static.nc $SLC_work/ocean_static.nc
     
     # Run sea level change tool
     slc_run
 
     # If change mask of altered cells has been generated, save figure properly.
     if [-f chng_mask.pdf]; then
-        mv chng_mask.pdf ${diag_dir}/MOM6_run${RUN}/chng_mask_${RUN}.pdf
-    
-    
-    # 
+        mv chng_mask.pdf ${diag_dir}/MOM6_run_${RUN}/chng_mask_${RUN}.pdf
+    # In any case, copy restarts and topography back to INPUT for restart.
+    cp ${SLC_work}/{MOM.res.nc,ice_model.res.nc,coupler.res,topog.nc} INPUT
 done
 
 
