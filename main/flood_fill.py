f5c5cb16aa40ac182a2e3043e39b490bb80407b7
Willem Huiskamp <willemhuiskamp88@gmail.com>
2024-04-19 14:40:34 +0200

    Updated to better floodfill algorithm

--- main/flood_fill.py
+++ main/flood_fill.py
@@ -1,23 +1,30 @@
-def flood_fill(col,row,ocean,filled,iso_mask):
-    # Recursive function to fill in the ocean and, as a result
-    # identify isolated cells created during the wetting/drying 
-    # in the previous steps.
-    # row,col  = Initial row/col values which we are certain are open ocean
-    # ocean    = Value of open ocean in the o_mask, in this case, 1
-    # filled   = Value of filled ocean, in this case 0
-    # iso_mask = The field we want to run on. Should be a (deep)copy
-    #              of the new ocean mask.  
+def flood_fill(i, j, wet, omask, xcyclic=True, tripolar=True):
+    #
+    # An iterative (stack based) flood fill algorithm. This is a modified
+    # version of the algorithm found in NCAR's topo edit notebook.
+    # Link: https://github.com/NCAR/tx2_3/blob/main/topography/MaskEdit_tx2_3v2b.ipynb
     # 
-    # Check we're inside the model domain
-    if col < 0 or col >= len(iso_mask[1]) or row < 0 or row >= len(iso_mask[0]):
-        return
-    # Is the current cell ocean? If not, return
-    if iso_mask[row,col] != ocean:
-        return
-    # If so, fill
-    iso_mask[row,col] = filled
-    # fourthly, attempt to fill the neighboring positions
-    flood_fill(col+1,row,ocean,filled,iso_mask)
-    flood_fill(col-1,row,ocean,filled,iso_mask)
-    flood_fill(col,row+1,ocean,filled,iso_mask)
-    flood_fill(col,row-1,ocean,filled,iso_mask)
+    # The flood fill starts at [j,i] and treats any positive value of "omask" as
+    # passable. Zero and negative values block flooding.
+
+    # xcyclic = True allows cyclic behavior in the last index. (default)
+    # tripolar = True allows a fold across the top-most edge. (default)
+    #
+    # Returns an array of 0's (not wet) and positive integers (defined by 'wet' - wet).
+    #
+    iso_mask = 0*omask
+    (nj,ni) = iso_mask.shape
+    stack = set()
+    stack.add( (j,i) )
+    while stack:
+        (j,i) = stack.pop()
+        if iso_mask[j,i] or omask[j,i] <= 0: continue
+        iso_mask[j,i] = wet
+        if i>0: stack.add( (j,i-1) )
+        elif xcyclic: stack.add( (j,ni-1) )
+        if i<ni-1: stack.add( (j,i+1) )
+        elif xcyclic: stack.add( (j,0) )
+        if j>0: stack.add( (j-1,i) )
+        if j<nj-1: stack.add( (j+1,i) )
+        elif tripolar: stack.add( (j,ni-1-i) ) # Tri-polar fold
+    return iso_mask
